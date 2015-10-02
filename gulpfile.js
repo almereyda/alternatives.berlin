fb80df75f7cfae4fbab7e25b34dfd18917ae3dec
jon r <almereyda@neocortex.circus>
2015-10-03 01:50:33 +0200

    deployment automatisation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp');
 
-gulp.task('dist', function() {
-  return gulp.src('node_modules/tangram/dist/tangram.min.js')
-    .pipe(gulp.dest('lib'));
+build_path = '_public';
+
+gulp.task('index', function() {
+  return gulp.src([
+      'index.html',
+      'main.js',
+      'scene.yaml'])
+    .pipe(gulp.dest(build_path));
 });
 
-gulp.task('default', ['dist']);
+gulp.task('libs', function() {
+  return gulp.src('lib/**')
+    .pipe(gulp.dest(build_path + '/lib'));
+});
+
+gulp.task('tangram', function() {
+  return gulp.src('node_modules/tangram/dist/tangram.min.js')
+    .pipe(gulp.dest(build_path + '/lib'));
+});
+
+gulp.task('build', ['index', 'libs', 'tangram']);
+
+gulp.task('default', ['build']);
