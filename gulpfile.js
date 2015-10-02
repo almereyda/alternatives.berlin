69095f5ece6e3c9deab7020fbdd4daded4af1091
jon r <almereyda@neocortex.circus>
2015-10-03 01:16:36 +0200

    regen for 64b9bf4

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('dist', function() {
-  return gulp.src('node_modules/tangram/dist/tangram.min.js')
-    .pipe(gulp.dest('lib'));
-});
-
-gulp.task('default', ['dist']);
